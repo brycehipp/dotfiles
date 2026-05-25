c3ff5cfba3d30df51eb22319e7178610898eb491
Bryce Hipp <brycehipp0@gmail.com>
2026-05-25 12:17:21 -0500

    refactor(brew): don't store casks in user app dir

--- shell/brew.zsh
+++ shell/brew.zsh
@@ -1,7 +1,5 @@
 export HOMEBREW_NO_ANALYTICS=1
 export HOMEBREW_NO_ENV_HINTS=1
-# use user's Applications dir for casks/apps
-export HOMEBREW_CASK_OPTS="--appdir=~/Applications"
 
 if command -v brew >/dev/null 2>&1; then
   local homebrew_dir="$(brew --prefix)"
